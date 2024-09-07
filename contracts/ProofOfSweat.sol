64aa77f2eef8a752905d2a27da82b9596f261603
ighmaz <ighmazcool@gmail.com>
2024-09-07 14:40:02 +0530

    add proof of sweat solidity code

---
+++ contracts/ProofOfSweat.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ProofOfSweat is ERC721URISTORAGE, Ownable {
+    unit256 public tokenCounter;
+
+    constructor() ERC721("ProofOfSweat", "POS") {
+        tokenCounter = 0;
+    }
+
+    function mintBatch(address to, string memory tokenURI) public onlyOwner {
+        _safeMint(to, tokenCounter);
+        _setTokenURI(tokenCounter, tokenURI);
+        tokenCounter++;
+    }
+}
